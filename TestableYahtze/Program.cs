254b6a7ac5bca6c903e2fdf09959bb378077423f
Nate Adams <psiberknetic@gmail.com>
2019-11-19 10:19:45 -0600

    Add project files.

---
+++ TestableYahtze/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace TestableYahtze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A bunch of Yahtze code...
+            //Player has rolled dice and now has the dice they'd like to score
+
+        }
+    }
+}
---
+++ YahzeeOriginal/Die.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dice
+{
+    public class Die
+    {
+        public Die(int sides = 6)
+        {
+            if (sides <= 0)
+            {
+                throw new ArgumentException("Sides must be greater than zero", "sides");
+            }
+
+            Id = Guid.NewGuid();
+            Sides = sides;
+            Roll();
+        }
+
+        private readonly Random _random = new Random();
+        private int _value;
+
+        public Guid Id { get; }
+
+        public int Value => _value;
+
+        public int Sides { get; }
+
+        public Die Roll()
+        {
+            _value = _random.Next(1, Sides);
+            return this;
+        }
+    }
+}
