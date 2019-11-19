d822daebebcf46558e89af263eed7effd8d1c1f3
Nate Adams <psiberknetic@gmail.com>
2019-11-19 11:58:00 -0600

    Implement program code

--- TestableYahtze/Program.cs
+++ TestableYahtze/Program.cs
@@ -8,7 +8,18 @@ namespace TestableYahtze
     {
         static void Main(string[] args)
         {
-
+            //A bunch of Yahtze code...
+            //Player has rolled dice and now has the dice they'd like to score
+            var roll = Enumerable.Range(1, 5).Select(_ => new Die());
+            if (roll.IsYahtze())
+            {
+                //score as Yahtze
+            }
+            else if (roll.IsLargeStraight())
+            {
+                // score as Full House
+            }
+            //etc...
         }
     }
 }
--- TestableYahtzeTests/TestingD6.cs
+++ TestableYahtzeTests/TestingD6.cs
@@ -5,6 +5,9 @@ using TestableYahtze;
 
 namespace TestableYahtzeTests
 {
+    // This is "poor man's Mocking" This works great on a small scale, but creating
+    // these classes on a large scale doesn't work well. We'll talk about Mocking 
+    // frameworks
     public class TestingD6 : IDie
     {
         public TestingD6(int value)
