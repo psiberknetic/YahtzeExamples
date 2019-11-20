6a16fbccdced398e9d6312d9322898001ac653f1
Nate Adams <psiberknetic@gmail.com>
2019-11-20 13:35:38 -0600

    Add Die Extensions class

---
+++ TestableYahtze/DieExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestableYahtze
+{
+    public static class DieExtensions
+    {
+        public static int GetTotalBySide(this IEnumerable<IDie> dice, int side)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetDieCountBySide(this IEnumerable<IDie> dice, int side)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
