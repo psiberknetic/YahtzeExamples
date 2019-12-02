3f32b9a7b496df2d42f2eb3f90ffe621a744fc34
Nate Adams <psiberknetic@gmail.com>
2019-12-02 10:22:36 -0600

    Refactor code out of Program

--- YahzeeOriginal/Program.cs
+++ YahzeeOriginal/Program.cs
@@ -8,41 +8,9 @@ namespace YahzeeOriginal
     {
         static void Main(string[] args)
         {
-            //A bunch of Yahtze code...
-            //Player has rolled dice and now has the dice they'd like to score
-            var dice = Enumerable.Range(1, 5).Select(_ => new Die());
-            if (IsYahtze(dice))
-            {
-                //score as Yahtze
-            }
-            else if (IsFullHouse(dice))
-            {
-                // score as Full House
-            }
-            //etc...
+
         }
 
-        // Note these private functions are very difficult to test. We can't write tests for 
-        // private functions, and making them public breaks encapsulation
 
-        // Also, the randomness of the die implementation makes these very difficult to test
-        private static bool IsYahtze(IEnumerable<Die> dice)
-        {
-            var dieGroups = dice.GroupBy(d => d.Value);
-            return dieGroups.Count() == 1;
-        }
-
-        private static bool IsFullHouse(IEnumerable<Die> dice)
-        {
-            var dieGroupCounts = dice.GroupBy(d => d.Value).Select(g => g.Count());
-            if (dieGroupCounts.Contains(2) && dieGroupCounts.Contains(3))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        // etc... other Yahtze functions
     }
 }
